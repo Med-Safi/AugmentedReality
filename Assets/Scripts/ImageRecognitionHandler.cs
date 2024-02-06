5a2078cc527121ed528554474e24a3177f0ee8a0
Med Safi <safii.med76@gmail.com>
2024-02-06 20:15:52 +0100

    DynLoadImage

--- Assets/Scripts/ImageRecognitionHandler.cs
+++ Assets/Scripts/ImageRecognitionHandler.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.XR.ARFoundation;
 
 public class ImageRecognitionHandler : MonoBehaviour
 {
     public ARTrackedImageManager trackedImageManager;
-    public MoveCubes moveCubesScript; // Reference to the MoveCubes script
+    public MoveCubes moveCubesScript;
+    public string imageUrl = "https://mix-ar.ru/content/ios/marker.jpg ";
+    public Renderer targetRenderer;
 
     private void Awake()
     {
@@ -29,6 +32,22 @@ public class ImageRecognitionHandler : MonoBehaviour
         {
             // Assuming you want to double the speed for any recognized image
             moveCubesScript.DoubleSpeed();
+            StartCoroutine(DownloadImage(imageUrl));
+        }
+    }
+    IEnumerator DownloadImage(string url)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Texture2D texture = DownloadHandlerTexture.GetContent(request);
+            targetRenderer.material.mainTexture = texture;
+        }
+        else
+        {
+            Debug.LogError("Error downloading image: " + request.error);
         }
     }
 }
